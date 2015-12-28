04858b88a9efc4940d1954ce3bff7b5e59a7d782
Florian Simon <florian@tentwentyfour.lu>
2015-12-28 17:14:09 +0100

    Changed the 'watch' task's name

--- scripts/gulp/meta.js
+++ scripts/gulp/meta.js
@@ -4,6 +4,6 @@
 
 var gulp = require('gulp');
 
-gulp.task('live-reload', function () {
+gulp.task('live-rebuild', function () {
     gulp.watch(['lib/assets/**/*', 'lib/templates/*'], ['create-root']);
 });
