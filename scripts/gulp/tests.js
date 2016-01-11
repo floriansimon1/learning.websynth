5cecad4379f19a9462200461494ba6e1bba4ab4b
Florian Simon <florian@tentwentyfour.lu>
2016-01-11 21:06:03 +0100

    Introduced jasmine tests

---
+++ scripts/gulp/tests.js
@@ -0,0 +1,11 @@
+/**
+ * Tasks related to testing.
+ */
+
+const gulp    = require('gulp');
+const jasmine = require('gulp-jasmine');
+
+gulp.task('test', function () {
+	gulp.src('tests/jasmine/**/*.js')
+	.pipe(jasmine());
+});
