04600d67893c78a9948af681e0e326f0483b76c4
Florian Simon <florian@tentwentyfour.lu>
2016-01-12 09:08:39 +0100

    WIP

--- scripts/gulp/scaffolding.js
+++ scripts/gulp/scaffolding.js
@@ -1,143 +1,134 @@
-const fs     = require('fs');
-const gulp   = require('gulp');
-const tasks  = {
+const gulp    = require('gulp');
+const readdir = require('recursive-readdir');
+const source  = require('vinyl-source-stream');
+
+const tasks   = {
     concat      : require('gulp-concat'),
-	runSequence : require('run-sequence'),
-    replace     : require('gulp-replace')
+    runSequence : require('run-sequence'),
+    replace     : require('gulp-replace'),
+    browserify  : require('browserify'),
+    glob        : require('glob')
 };
 
 const vendorCss = [
     'node_modules/materialize-css/bin/materialize.css'
 ];
 
-const vendorJs = [
-	'node_modules/jquery/dist/jquery.js',
-    'node_modules/materialize-css/bin/materialize.js',
-	'node_modules/lodash/index.js',
-	'node_modules/bacon/dist/Bacon.js',
-	'node_modules/react/dist/react.js',
-	'node_modules/react-stampit/dist/react-stampit.js',
-	'node_modules/react-hyperscript/index.js',
-	'node_modules/react-router/umd/ReactRouter.js',
-	'node_modules/di4js/lib/di4js.js'
-];
-
 /* Paths and file names. */
-const rootFolder            = 'web/';
-const webJsFolder           = 'app/';
-const webAppSubFolder       = 'client/';
-const assetsSubFolder       = 'assets/';
-const sourceFolder          = 'source/';
-const vendorJsFileName      = 'vendor.js';
-const vendorCssFileName     = 'vendor.css';
-const rootJsFolder          = rootFolder + webJsFolder;
-const cssAssetsSubFolder    = assetsSubFolder + 'css/';
-const templatesBase         = sourceFolder + 'templates/';
-const rootAssetsFolder      = rootFolder + assetsSubFolder;
-const sourceAssetsFolder    = sourceFolder + assetsSubFolder;
-const rootCssFolder         = rootFolder + cssAssetsSubFolder;
-const sourceCssAssetsFolder = sourceFolder + cssAssetsSubFolder;
-const sourceWebAppFolder    = sourceFolder + webAppSubFolder
-const sourceWebAppFiles     = sourceWebAppFolder + '/**/*';
-const sourceAssets          = sourceAssetsFolder + '/**/*';
-const indexHtmlTemplate     = templatesBase + 'index.html';
-const indexHtmlFile         = rootFolder + 'index.html';
+const indexHtmlTemplate          = 'source/templates/index.html';
+const sourceCssFolder            = 'source/assets/css/';
+const sourceWebAppFolder         = 'source/client/';
+const vendorCssFile              = 'vendor.css';
+const cssDestinationFolder       = 'web/css';
+const webAppDestinationFileName  = 'app.js';
+const webAppDestinationFolder    = 'web/';
+const indexHtmlDestinationFolder = 'web/';
+const cssUrl                     = 'css/';
 
-/* Reads files in a folder and returns them in a list. */
-const getAndConvertFileNames = function (directory, additional, template, joinWith) {
-    return [additional]
-	.concat(fs.readdirSync(directory))
+/* Returns the parent directory of a file. */
+const parent = file => file.substring(0, file.lastIndexOf('/') + 1);
+
+/* Concatenates a list of files into a string to embed in a HTML file. */
+const filesListToString = function (files, template, joinWith) {
+    return files
     .map(file => template.replace('%s', file))
     .join(joinWith);
 };
 
-/* Gulp helper to replace a string in index.html. */
-const replaceInFile = function (file, what, replacement) {
+/* Reads files in a folder and returns them in a list. */
+const getFiles = function (directory) {
+    return new Promise(function (resolve, reject) {
+        readdir(directory, function (error, files) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(files);
+            }
+        });
+    });
+};
+
+/* Gulp helper to replace a string in a file. */
+const replaceInFile = function (file, destination, what, replacement) {
     return gulp
     .src(file)
     .pipe(tasks.replace(what, replacement))
-    .pipe(gulp.dest(file.substring(0, file.lastIndexOf('/') + 1)));
+    .pipe(gulp.dest(destination));
 };
 
-/* Copies concatenated vendor files into the root folder. */
-const concatAndCopyVendorFiles = function (vendorFiles, outputFileName, destinationFolder) {
+/* Copies all vendor css files to the web folder. */
+gulp.task('copy-vendor-css', function (vendorFiles, outputFileName, destinationFolder) {
     return function () {
         return gulp
-        .src(vendorFiles)
-        .pipe(tasks.concat(outputFileName))
-        .pipe(gulp.dest(destinationFolder));
+        .src(vendorCss)
+        .pipe(tasks.concat(vendorCssFile))
+        .pipe(gulp.dest(cssDestinationFolder));
     };
-};
+});
 
-/* Copies all vendor assets to the web folder. */
-gulp.task('copy-vendor-css', concatAndCopyVendorFiles(vendorCss, vendorCssFileName, rootCssFolder));
-gulp.task('copy-vendor-js', concatAndCopyVendorFiles(vendorJs, vendorJsFileName, rootJsFolder));
-
-/* Takes all assets files and places them into the web root. */
-gulp.task('copy-assets', function () {
-    return gulp
-    .src(sourceAssets, { base : sourceAssetsFolder })
-    .pipe(gulp.dest(rootAssetsFolder));
+/* Copies all of our own css files to the web folder. */
+gulp.task('copy-vendor-css', function (vendorFiles, outputFileName, destinationFolder) {
+    return function () {
+        return gulp
+        .src(sourceCssFolder + '/**/*.css', { base : sourceCssFolder })
+        .pipe(gulp.dest(cssDestinationFolder));
+    };
 });
 
 /* Takes all assets files and places them into the web root. */
-gulp.task('copy-app', function () {
-    return gulp
-    .src(sourceWebAppFiles, { base : sourceWebAppFolder })
-    .pipe(gulp.dest(rootJsFolder));
+gulp.task('copy-web-app', function () {
+    const files = tasks.glob.sync(sourceWebAppFolder + '/**/*.js');
+
+    return tasks.browserify({ entries : files })
+    .bundle()
+    .pipe(source(webAppDestinationFileName))
+    .pipe(gulp.dest(webAppDestinationFolder));
 });
 
 /* Adds the index HTML template. */
 gulp.task('copy-templates', function () {
     return gulp
-    .src(indexHtmlTemplate, { base : templatesBase })
-    .pipe(gulp.dest(rootFolder));
+    .src(indexHtmlTemplate, { base : parent(indexHtmlTemplate) })
+    .pipe(gulp.dest(indexHtmlDestinationFolder));
 });
 
 /* Adds CSS files to the index.html template. */
-gulp.task('add-css', function () {
-    return replaceInFile(
-		indexHtmlFile,
-        '%STYLES%',
-        getAndConvertFileNames(
-            sourceCssAssetsFolder,
-            [vendorCssFileName],
-            (
+gulp.task('insert-css', function () {
+    return getFiles(sourceCssFolder)
+    .then(function (files) {
+        replaceInFile(
+            indexHtmlTemplate,
+            indexHtmlDestinationFolder,
+            '%STYLES%',
+            filesListToString(
+                files,
                 '<link href="/'
-                + cssAssetsSubFolder
-                + '%s" rel="stylesheet" type="text/css" />'
-            ),
-            '\n        '
-        )
-    );
+                + cssUrl
+                + '%s" rel="stylesheet" type="text/css" />',
+                '\n        '
+            )
+        );
+    });
 });
 
 /* Adds JS files to the index.html template. */
-gulp.task('add-js', function () {
+gulp.task('insert-js', function () {
     return replaceInFile(
-		indexHtmlFile,
+        indexHtmlTemplate,
+        indexHtmlDestinationFolder,
         '%SCRIPTS%',
-        getAndConvertFileNames(
-            sourceWebAppFolder,
-            [vendorJsFileName],
-            (
-                '<script type="text/javascript" src="'
-                + webJsFolder
-                + '%s"></script>'
-            ),
-            '\n        '
+        (
+            '<script type="text/javascript" src="'
+            + webAppDestinationFileName + '">'
+            + '</script>'
         )
     );
 });
 
 /* Processes template files and saves them in the root. */
 gulp.task('process-templates', function () {
-	return tasks.runSequence('copy-templates', 'add-css', 'add-js');
+    return tasks.runSequence('copy-templates', 'insert-css', 'insert-js');
 });
 
 /* Task that puts assets in the web folder and transforms them. */
-gulp.task('create-root', [
-    'copy-vendor-css',  'copy-vendor-js',
-    'copy-assets',      'copy-app',
-    'process-templates'
-]);
+gulp.task('create-root', ['copy-vendor-css', 'copy-web-app', 'process-templates']);
