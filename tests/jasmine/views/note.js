5970dc9bf176e5bd267e6204c98c05ec90a823d9
Florian Simon <florian@tentwentyfour.lu>
2016-03-01 21:55:55 +0100

    More Note tests

--- tests/jasmine/views/note.js
+++ tests/jasmine/views/note.js
@@ -1,6 +1,7 @@
 const sandal = require('../../../source/client/providers');
 const Note   = require('../../../source/client/views/note');
 const Maybe  = require('data.maybe');
+const _      = require('lodash');
 
 const position   = 3;
 const spies      = {
@@ -27,4 +28,23 @@ describe('The Note component', () => {
 
         expect(spies.toggle).toHaveBeenCalledWith(instrument, position);
     });
+
+    it('should get styled differently when it is played than when it is not', () => {
+        const played    = _.range(2).map(() => Note(Maybe.of(0), spies.toggle, instrument, 0));
+        const notPlayed = _.range(2).map(() => Note(Maybe.of(0), spies.toggle, instrument, 1));
+
+        expect(played[0].properties.style.backgroundColor).toEqual(
+            played[1].properties.style.backgroundColor
+        );
+
+        expect(notPlayed[0].properties.style.backgroundColor).toEqual(
+            notPlayed[1].properties.style.backgroundColor
+        );
+
+        expect(played[0].properties.style.backgroundColor).not.toEqual(
+            notPlayed[0].properties.style.backgroundColor
+        );
+
+        expect(played)
+    })
 });
