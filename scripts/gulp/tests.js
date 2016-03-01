82a4853f41eda072242277d77b30c579a897b64a
Florian Simon <florian@tentwentyfour.lu>
2016-03-01 21:16:19 +0100

    Setting up istanbul for code coverage

--- scripts/gulp/tests.js
+++ scripts/gulp/tests.js
@@ -2,10 +2,22 @@
  * Tasks related to testing.
  */
 
-const gulp    = require('gulp');
-const jasmine = require('gulp-jasmine');
+const gulp     = require('gulp');
+const jasmine  = require('gulp-jasmine');
+const istanbul = require('gulp-istanbul');
 
-gulp.task('test', function () {
-    gulp.src('tests/jasmine/**/*.js')
-    .pipe(jasmine({ verbose: true }));
-});
+gulp.task('test', ['coverage'], () => (
+    gulp
+    .src('tests/jasmine/**/*.js')
+    .pipe(jasmine({ verbose: true }))
+    .pipe(istanbul.writeReports({
+        reportOpts: { dir: 'reports/coverage' },
+    }))
+));
+
+gulp.task('coverage', () => (
+    gulp
+    .src('source/client/**/*.js ')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+));
