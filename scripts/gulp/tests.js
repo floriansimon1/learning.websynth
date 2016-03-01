0994da481386804d46632f14a4a3874a14baf8db
Florian Simon <florian@tentwentyfour.lu>
2016-03-01 21:27:23 +0100

    ES6 in gulpfiles makes Mr. Travis angry

--- scripts/gulp/tests.js
+++ scripts/gulp/tests.js
@@ -6,18 +6,18 @@ const gulp     = require('gulp');
 const jasmine  = require('gulp-jasmine');
 const istanbul = require('gulp-istanbul');
 
-gulp.task('test', ['coverage'], () => (
-    gulp
+gulp.task('test', ['coverage'], function () {
+    return gulp
     .src('tests/jasmine/**/*.js')
     .pipe(jasmine({ verbose: true }))
     .pipe(istanbul.writeReports({
         reportOpts: { dir: 'reports/coverage' },
     }))
-));
+});
 
-gulp.task('coverage', () => (
-    gulp
+gulp.task('coverage', function () {
+    return gulp
     .src('source/client/**/*.js ')
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
-));
+});
